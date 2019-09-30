ef5a5c2f134505add3751b92a3c0966094ec3f67
Mohammed Amarnah <m.amarnah@gmail.com>
2019-09-30 22:43:14 +0300

    drawing lines as a collider and small movement enhancements

--- Assets/Scripts/AmebaMovement.cs
+++ Assets/Scripts/AmebaMovement.cs
@@ -1,21 +1,26 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class AmebaMovement : MonoBehaviour {
   public static float speed = 0.1f;
+  public static EdgeCollider2D line = new EdgeCollider2D();
+  bool stop = false;  
 
   void Update() {
-    // Debug.Log(speed);
-    float newX = UnityEngine.Random.Range(-2.5f, 2.5f);
-    float newY = UnityEngine.Random.Range(-4.5f, 4.5f);
-    Vector3 newPos = new Vector3(newX, newY, transform.position.z);
-    Vector3 pos = transform.position;
-    if (speed == 0.1f) {
-      transform.Translate(newPos * Time.deltaTime * speed, Space.Self);
-    } else {
-      transform.position = Vector3.Lerp(pos, newPos, speed * Time.deltaTime);
+    // Construct a Point() from the position of the current ameba
+    // if SpeedController.polygon != null && !isClosed
+    // and Geometry.PointInConvexPolygon(colliderPolygon, currentPosition)
+    // then stop = true and we can destroy the ameba
+    // and increase the score
+    if (!stop) {
+      float newX = UnityEngine.Random.Range(-2.5f, 2.5f);
+      float newY = UnityEngine.Random.Range(-4.5f, 4.5f);
+      Vector3 newPos = new Vector3(newX, newY, transform.position.z);
+      Vector3 pos = transform.position;
+      if (speed == 0.1f) {
+        transform.Translate(newPos * Time.deltaTime * speed, Space.Self);
+      } else {
+        transform.position = Vector3.Lerp(pos, newPos, speed * Time.deltaTime);
+      }
     }
   }
 }
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+  [SerializeField]
+  public GameObject linePrefab;
+  
+  [NonSerialized]
+  public static GameObject currentLine;
+
+  [NonSerialized]
+  public List<Vector2> fingerPositions = new List<Vector2>();
+
+  [NonSerialized]
+  public LineRenderer lineRenderer;
+  [NonSerialized]
+  public EdgeCollider2D edgeCollider;
+  [NonSerialized]
+  public Polygon polygon;
+
+  void Update() {
+    if(Input.GetMouseButtonDown(0)) {
+      AmebaMovement.speed = 0.3f;
+      CreateLine();
+    }
+    if(Input.GetMouseButton(0) && currentLine != null) {
+      Vector2 tempFingerPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+      if(Vector2.Distance(tempFingerPos, fingerPositions.Last()) > 0.1f) {
+        UpdateLine(tempFingerPos);
+      }
+    }
+    if (Input.GetMouseButtonUp(0)) {
+      AmebaMovement.speed = 0.1f;
+    }
+  }
+
+  void CreateLine() {
+    currentLine = Instantiate(linePrefab, Vector3.zero, Quaternion.identity);
+    lineRenderer = currentLine.GetComponent<LineRenderer>();
+    edgeCollider = currentLine.GetComponent<EdgeCollider2D>();
+    polygon = currentLine.GetComponent<Polygon>();
+    fingerPositions.Clear();
+    fingerPositions.Add(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+    fingerPositions.Add(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+    lineRenderer.SetPosition(0, fingerPositions[0]);
+    lineRenderer.SetPosition(1, fingerPositions[1]);
+    edgeCollider.points = fingerPositions.ToArray();
+    AmebaMovement.line = edgeCollider;
+    StartCoroutine(DestroyAfter(1f));
+  }
+
+  void UpdateLine(Vector2 fingerPos) {
+    if (currentLine != null) {
+      fingerPositions.Add(fingerPos);
+      lineRenderer.positionCount++;
+      lineRenderer.SetPosition(lineRenderer.positionCount - 1, fingerPos);
+      edgeCollider.points = fingerPositions.ToArray();
+      AmebaMovement.line = edgeCollider;
+    }
+  }
+
+  IEnumerator DestroyAfter(float seconds) {
+    yield return new WaitForSeconds(seconds);
+    if (currentLine != null && !polygon.isClosed) {
+      Destroy(currentLine);
+      currentLine = null;
+    }
+  }
+}
