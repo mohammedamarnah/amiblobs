3f49abd36dc697b49e3f4ada8c39c295495d1824
Mohammed Amarnah <m.amarnah@gmail.com>
2019-09-30 14:37:55 +0300

    initial movement code and prefabs

---
+++ Assets/AmebaMovement.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmebaMovement : MonoBehaviour {
+  public static float speed = 0.1f;
+
+  void Update() {
+    // Debug.Log(speed);
+    float newX = UnityEngine.Random.Range(-2.5f, 2.5f);
+    float newY = UnityEngine.Random.Range(-4.5f, 4.5f);
+    Vector3 newPos = new Vector3(newX, newY, transform.position.z);
+    Vector3 pos = transform.position;
+    if (speed == 0.1f) {
+      transform.Translate(newPos * Time.deltaTime * speed, Space.Self);
+    } else {
+      transform.position = Vector3.Lerp(pos, newPos, speed * Time.deltaTime);
+    }
+  }
+}
---
+++ Assets/SpeedController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class SpeedController : MonoBehaviour, IPointerUpHandler, IPointerDownHandler {
+    public void OnPointerDown(PointerEventData _) {
+        AmebaMovement.speed = 0.3f;
+    }
+    public void OnPointerUp(PointerEventData _) {
+        AmebaMovement.speed = 0.1f;
+    }
+}
